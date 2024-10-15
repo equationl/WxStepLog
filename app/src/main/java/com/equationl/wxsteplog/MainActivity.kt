750a1ed1e775ef6255f75b1ae38fc5a0118c003e
equationl <equationl@qq.com>
2024-10-15 17:24:56 +0800

    feat: 支持导出数据

--- app/src/main/java/com/equationl/wxsteplog/MainActivity.kt
+++ app/src/main/java/com/equationl/wxsteplog/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.equationl.wxsteplog
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,13 +24,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.LifecycleResumeEffect
+import com.equationl.wxsteplog.db.DbUtil
 import com.equationl.wxsteplog.model.WxStepLogSetting
 import com.equationl.wxsteplog.step.OverManager
 import com.equationl.wxsteplog.ui.theme.WxStepLogTheme
+import com.equationl.wxsteplog.util.DateTimeUtil.formatDateTime
 import com.equationl.wxsteplog.util.log.Logger
 import com.ven.assists.Assists
 import com.ven.assists.AssistsService
@@ -35,6 +42,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity(), AssistsServiceListener {
     private val isAccessibilityServiceEnabled = mutableStateOf(false)
@@ -112,7 +120,15 @@ class MainActivity : ComponentActivity(), AssistsServiceListener {
 @Composable
 private fun HomePage(isAccessibilityServiceEnabled: MutableState<Boolean>) {
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
     val userName = remember { mutableStateOf("") }
+    val exportLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        scope.launch(Dispatchers.IO) {
+            exportData(result, context)
+        }
+    }
 
     LifecycleResumeEffect(Unit) {
         isAccessibilityServiceEnabled.value = Assists.isAccessibilityServiceEnabled()
@@ -153,6 +169,22 @@ private fun HomePage(isAccessibilityServiceEnabled: MutableState<Boolean>) {
         ) {
             Text(if (isAccessibilityServiceEnabled.value) "开始记录" else "打开无障碍服务")
         }
+
+        TextButton(
+            onClick = {
+                val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "text/comma-separated-values"
+                    putExtra(Intent.EXTRA_TITLE, "wxStepLog_${System.currentTimeMillis().formatDateTime("yyyy_MM_dd_HH_mm_ss")}.csv")
+                }
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                intent.setFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+                intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                exportLauncher.launch(intent)
+            }
+        ) {
+            Text("导出数据为 csv")
+        }
     }
 }
 
@@ -168,4 +200,22 @@ private fun HomeSettingContent(userName: MutableState<String>) {
             Text(text = "请输入要记录的微信用户名或备注名")
         },
     )
+}
+
+private suspend fun exportData(result: ActivityResult, context: Context) {
+    val data = result.data
+    val uri = data?.data
+    uri?.let {
+        context.contentResolver.openOutputStream(it)?.let {outputStream ->
+            val dataList = DbUtil.db.manHoursDB().queryAllData()
+            for (row in dataList) {
+                outputStream.write("${row.id},${row.userName},${row.stepNum},${row.likeNum},${row.logTimeString},${row.logTime}\n".toByteArray())
+            }
+            outputStream.flush()
+            outputStream.close()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "导出完成！", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
--- app/src/main/java/com/equationl/wxsteplog/db/DBDao.kt
+++ app/src/main/java/com/equationl/wxsteplog/db/DBDao.kt
@@ -3,9 +3,13 @@ package com.equationl.wxsteplog.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface WxStepDao{
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertData(data: WxStepTable): Long
+
+    @Query("SELECT * FROM wx_step_table")
+    suspend fun queryAllData(): List<WxStepTable>
 }
