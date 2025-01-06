595bc166540aad3deb6f504c42771233b93c0f2b
equationl <equationl@qq.com>
2025-01-06 11:19:59 +0800

    bump 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.equationl.wxsteplog"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
